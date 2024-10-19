018dc14bb14f1d65487d82e371ab536c9785b53b
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-10-19 11:28:27 +0200

    Add files via upload

---
+++ PartitionAlgorithms/partition_point/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+auto print_seq = [](auto rem, auto first, auto last)
+    {
+        for (cout << rem; first != last; cout << *first++ << ' ') {}
+        cout << endl;
+    };
+
+int main()
+{
+    array v{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    auto is_even = [](int i) { return i % 2 == 0; };
+
+    partition(v.begin(), v.end(), is_even);
+    print_seq("After partitioning, v: ", v.cbegin(), v.cend());
+
+    const auto pp = partition_point(v.cbegin(), v.cend(), is_even);
+    const auto i = distance(v.cbegin(), pp);
+    cout << "Partition point is at " << i << "; v[" << i << "] = " << *pp << endl;
+
+    print_seq("First partition (all even elements): ", v.cbegin(), pp);
+    print_seq("Second partition (all odd elements): ", pp, v.cend());
+   
+    return 0;
+}
